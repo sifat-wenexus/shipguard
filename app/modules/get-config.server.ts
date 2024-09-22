50f6d367370b7d6e190c2acc7212416680fae9e7
WeNexusTeam <wenexusltd@gmail.com>
2024-09-22 10:32:27 +0600

    use shopify api key from shopify.app.toml

--- app/modules/get-config.server.ts
+++ app/modules/get-config.server.ts
@@ -17,7 +17,7 @@ export function getConfig() {
 
   const shopifyAppTomlFile = process.env.NODE_ENV === 'production' ? 'shopify.app.prod.toml' : 'shopify.app.toml';
 
-  console.log('Loading Shopify app config from', shopifyAppTomlFile);
+  console.log('Loading Shopify app config from ', shopifyAppTomlFile);
 
   const shopifyAppToml = toml.parse(
     fs.readFileSync(path.resolve(__dirname, '../', shopifyAppTomlFile), 'utf-8'),
@@ -26,7 +26,7 @@ export function getConfig() {
   process.env.APP_URL = shopifyAppToml.application_url;
 
   _config = {
-    apiKey: process.env.SHOPIFY_API_KEY!,
+    apiKey: shopifyAppToml.client_id || process.env.SHOPIFY_API_KEY!,
     apiSecretKey: process.env.SHOPIFY_API_SECRET || '',
     scopes:
       shopifyAppToml.access_scopes?.scopes?.split(',') ?? process.env.SCOPES?.split(','),
@@ -34,8 +34,6 @@ export function getConfig() {
       shopifyAppToml.application_url ?? process.env.SHOPIFY_APP_URL ?? '',
     ),
   };
-
-  console.log('Shopify app config:', _config);
 
   return _config;
 }
