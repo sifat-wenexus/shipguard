d7211fe4488369b8fa466107e2cc4bb3fd648705
WeNexusTeam <wenexusltd@gmail.com>
2024-10-01 12:47:09 +0600

    add variable types

--- app/routes/settings.email-template/email-template/template.ts
+++ app/routes/settings.email-template/email-template/template.ts
@@ -1,6 +1,7 @@
-import { $Enums } from '#prisma-client';
-import { Liquid } from 'liquidjs';
 import { prisma } from '~/modules/prisma.server';
+import type { $Enums } from '#prisma-client';
+import { Liquid } from 'liquidjs';
+
 abstract class Template<C = any> {
   // abstract variables(context?: C): Promise<Record<string, any>>;
   abstract name: $Enums.EmailTemplateName;
@@ -28,27 +29,69 @@ abstract class Template<C = any> {
   }
 }
 
-export class ClaimRequestAdminTemplate extends Template {
+export interface ClaimRequestAdminTemplateVariables {
+  shop_name: string;
+  order_id: string;
+  customer_name: string;
+  claim_reason: string;
+  claim_date: string;
+  order_url: string;
+}
+
+export interface ClaimRequestCustomerTemplateVariables {
+  order_id: string;
+  customer_name: string;
+  claim_reason: string;
+  claim_date: string;
+  shop_name: string;
+  shop_logo: string;
+}
+
+export interface ClaimRefundCustomerTemplateVariables {
+  order_id: string;
+  refund_amount: string;
+  date: string;
+  shop_name: string;
+  shop_logo: string;
+}
+
+export interface ClaimReOrderCustomerTemplateVariables {
+  order_id: string;
+  replacement_order_id: string;
+  shipping_details: string;
+  shop_name: string;
+  shop_logo: string;
+}
+
+export interface ClaimCancelCustomerTemplateVariables {
+  order_id: string;
+  customer_name: string;
+  cancellation_reason: string;
+  shop_name: string;
+  shop_logo: string;
+}
+
+export class ClaimRequestAdminTemplate extends Template<ClaimRequestAdminTemplateVariables> {
   name: $Enums.EmailTemplateName = 'CLAIM_REQUEST_EMAIL_FOR_ADMIN';
   liquid = '';
 }
 
-export class ClaimRequestCustomerTemplate extends Template {
+export class ClaimRequestCustomerTemplate extends Template<ClaimRequestCustomerTemplateVariables> {
   name: $Enums.EmailTemplateName = 'CLAIM_REQUEST_EMAIL_FOR_CUSTOMER';
   liquid = '';
 }
-export class ClaimRefundCustomerTemplate extends Template {
+
+export class ClaimRefundCustomerTemplate extends Template<ClaimRefundCustomerTemplateVariables> {
   name: $Enums.EmailTemplateName = 'CLAIM_REFUND_EMAIL_FOR_CUSTOMER';
   liquid = '';
 }
-export class ClaimReOrderCustomerTemplate extends Template {
+
+export class ClaimReOrderCustomerTemplate extends Template<ClaimReOrderCustomerTemplateVariables> {
   name: $Enums.EmailTemplateName = 'CLAIM_REORDER_EMAIL_FOR_CUSTOMER';
   liquid = '';
 }
-export class ClaimCancelCustomerTemplate extends Template {
+
+export class ClaimCancelCustomerTemplate extends Template<ClaimCancelCustomerTemplateVariables> {
   name: $Enums.EmailTemplateName = 'CLAIM_CANCEL_EMAIL_FOR_CUSTOMER';
   liquid = '';
 }
-// const claimReqTemplate = new ClaimRequestAdminTemplate();
-
-// claimReqTemplate.render({});
