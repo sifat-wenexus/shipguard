a9d4b6be22c19d269ae626831d5e5ca33d904f06
sifat-wenexus <sifat@wenexus.io>
2024-10-31 16:55:02 +0600

    added log for live debug

--- app/listeners/shop.server.ts
+++ app/listeners/shop.server.ts
@@ -3,6 +3,7 @@ import { upsertStore } from '~/modules/init-store.server';
 import { emitter } from '~/modules/emitter.server';
 
 emitter.on('SHOP_UPDATE', async ({ session }: WebhookListenerArgs) => {
+  console.log('SHOP_UPDATE', session);
   if (session) {
     const res = await upsertStore(session);
 
