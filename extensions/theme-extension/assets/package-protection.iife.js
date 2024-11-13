4b9b9a82b7d1a32d93fcdd7e2109ab6915bb50b4
sifat-wenexus <sifat@wenexus.io>
2024-11-13 14:45:06 +0600

    bug fix on multiple shipping product add mini cart and cart page

--- extensions/theme-extension/assets/package-protection.iife.js
+++ extensions/theme-extension/assets/package-protection.iife.js
@@ -681,14 +681,15 @@ var __publicField = (obj, key, value) => {
       Array.from(element).forEach((form) => {
         form.addEventListener("submit", async (e) => {
           e.preventDefault();
-          console.log("event", form);
-          const checkoutButton = document.querySelector(
+          const checkoutButtons = document.querySelectorAll(
             "button[type='submit'][name='checkout']"
           );
-          checkoutButton.disabled = true;
+          checkoutButtons.forEach((button) => {
+            button.disabled = true;
+          });
           if (enabled() && v.length > 0) {
             await packageProtectionApi.add();
-            console.log("adding to cart2");
+            console.log("adding to cart -");
           } else {
             await packageProtectionApi.remove();
           }
