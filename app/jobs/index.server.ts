fff62beb8efe9d305f1eaee1faa9f9a59d47c7b0
WeNexusTeam <wenexusltd@gmail.com>
2024-11-20 12:32:20 +0600

    remove uncommitted file imports

--- app/jobs/index.server.ts
+++ app/jobs/index.server.ts
@@ -1,5 +1,4 @@
 import { UpdateProductCollection } from '~/jobs/update-product-collection';
-import { AnalyzeCurrentTheme } from '~/jobs/analyze-current-theme.server';
 import { ImportProducts } from '~/jobs/import-products.server';
 import { ImportOrders } from '~/jobs/import-orders.server';
 import { ShopRedact } from '~/jobs/shop-redact.server';
@@ -7,7 +6,6 @@ import { ShopRedact } from '~/jobs/shop-redact.server';
 
 export const jobExecutors = {
   'update-product-collection': UpdateProductCollection,
-  'analyze-current-theme': AnalyzeCurrentTheme,
   'import-products': ImportProducts,
   'import-orders': ImportOrders,
   'shop-redact': ShopRedact,
