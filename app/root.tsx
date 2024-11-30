e49caeccd1a5b06eec8446fa4f9c5ceba252a21c
sifat-wenexus <sifat@wenexus.io>
2024-11-30 18:05:40 +0600

    product image if not exist then create

--- app/root.tsx
+++ app/root.tsx
@@ -26,6 +26,8 @@ import {
   useLocation,
   useRouteError,
 } from '@remix-run/react';
+import { getShopifyGQLClient, shopify } from '~/modules/shopify.server';
+import { prisma } from '~/modules/prisma.server';
 
 export const links = () => [
   { rel: 'icon', href: '/favicon.png', type: 'image/png' },
@@ -43,7 +45,106 @@ const skipAuthPaths = new Set([
   '/',
 ]);
 
-export async function loader() {
+export async function loader({ request }) {
+  const ctx = await shopify.authenticate.admin(request);
+const productImage='https://cdn.shopify.com/s/files/1/0900/3221/0212/files/Inhouse_Shipping_Protection.png?v=1728361462'
+  const gqlClient = getShopifyGQLClient(ctx.session);
+
+    const existingProduct = await gqlClient.query<any>({
+      data: {
+        query: `#graphql
+      query Product {
+        products(first: 100 ,query:"tag:wenexus-insurance OR sku:wenexus-shipping-protection"){
+          nodes{
+            id
+            title
+            media(first: 200){
+              nodes{
+                preview{
+                  image{
+                    url
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+      `,
+      }
+    });
+
+    console.log('product-image', JSON.stringify(existingProduct.body.data.products));
+  for (const node of existingProduct.body.data.products.nodes) {
+    const mediaNodes = node.media?.nodes || [];
+
+    console.log('mediaNodes',node.media.nodes.length>0);
+
+    if(node.media.nodes.length===0){
+      await gqlClient.query<any>({
+        data: {
+          query: `#graphql
+          mutation productCreateMedia($media: [CreateMediaInput!]!, $productId: ID!) {
+            productCreateMedia(media: $media, productId: $productId) {
+
+              mediaUserErrors {
+                field
+                message
+              }
+
+            }
+          }`,
+          variables: {
+            media: [
+              {
+                alt: 'package-protection',
+                mediaContentType: 'IMAGE',
+                originalSource: productImage,
+              },
+            ],
+            productId: node.id,
+          },
+        },
+        tries: 20,
+      });
+    }
+    for (const media of mediaNodes) {
+      if (media.preview?.image?.url) {
+        console.log('product id----> ',node.id)
+        console.log("found:", media.preview.image.url);
+
+      } else {
+        console.log("not-found");
+        // await gqlClient.query<any>({
+        //   data: {
+        //     query: `#graphql
+        //     mutation productCreateMedia($media: [CreateMediaInput!]!, $productId: ID!) {
+        //       productCreateMedia(media: $media, productId: $productId) {
+        //
+        //         mediaUserErrors {
+        //           field
+        //           message
+        //         }
+        //
+        //       }
+        //     }`,
+        //     variables: {
+        //       media: [
+        //         {
+        //           alt: 'package-protection',
+        //           mediaContentType: 'IMAGE',
+        //           originalSource: productImage,
+        //         },
+        //       ],
+        //       productId: node.id,
+        //     },
+        //   },
+        //   tries: 20,
+        // });
+      }
+    }
+  }
+
   return json({ apiKey: process.env.SHOPIFY_API_KEY });
 }
 
