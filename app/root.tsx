9bf0194ff171ac25b6f6a543457f4de5e02a8d10
WeNexusTeam <wenexusltd@gmail.com>
2024-11-30 18:17:45 +0600

    update init

--- app/root.tsx
+++ app/root.tsx
@@ -3,13 +3,12 @@ import polarisViz from '@shopify/polaris-viz/build/esm/styles.css';
 import polarisStyles from '@shopify/polaris/build/esm/styles.css';
 import { queryProxy } from '~/modules/query/query-proxy';
 import { AppProvider } from '~/shopify-app-remix/react';
+import { useEffect, useMemo, useState } from 'react';
 import LogoAnimation from '~/components/page-loader';
 import tailwindStyles from '~/styles/tailwind.css';
 import { MainNav } from './components/main-nav';
-import { useEffect, useMemo, useState } from 'react';
 import { Frame } from '@shopify/polaris';
 import { json } from '@remix-run/node';
-import { Nav } from './components/nav';
 
 import appCss from '~/styles/app.css';
 import rsuite from 'rsuite/Accordion/styles/index.css';
@@ -26,8 +25,6 @@ import {
   useLocation,
   useRouteError,
 } from '@remix-run/react';
-import { getShopifyGQLClient, shopify } from '~/modules/shopify.server';
-import { prisma } from '~/modules/prisma.server';
 
 export const links = () => [
   { rel: 'icon', href: '/favicon.png', type: 'image/png' },
@@ -45,106 +42,7 @@ const skipAuthPaths = new Set([
   '/',
 ]);
 
-export async function loader({ request }) {
-  const ctx = await shopify.authenticate.admin(request);
-const productImage='https://cdn.shopify.com/s/files/1/0900/3221/0212/files/Inhouse_Shipping_Protection.png?v=1728361462'
-  const gqlClient = getShopifyGQLClient(ctx.session);
-
-    const existingProduct = await gqlClient.query<any>({
-      data: {
-        query: `#graphql
-      query Product {
-        products(first: 100 ,query:"tag:wenexus-insurance OR sku:wenexus-shipping-protection"){
-          nodes{
-            id
-            title
-            media(first: 200){
-              nodes{
-                preview{
-                  image{
-                    url
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-      `,
-      }
-    });
-
-    console.log('product-image', JSON.stringify(existingProduct.body.data.products));
-  for (const node of existingProduct.body.data.products.nodes) {
-    const mediaNodes = node.media?.nodes || [];
-
-    console.log('mediaNodes',node.media.nodes.length>0);
-
-    if(node.media.nodes.length===0){
-      await gqlClient.query<any>({
-        data: {
-          query: `#graphql
-          mutation productCreateMedia($media: [CreateMediaInput!]!, $productId: ID!) {
-            productCreateMedia(media: $media, productId: $productId) {
-
-              mediaUserErrors {
-                field
-                message
-              }
-
-            }
-          }`,
-          variables: {
-            media: [
-              {
-                alt: 'package-protection',
-                mediaContentType: 'IMAGE',
-                originalSource: productImage,
-              },
-            ],
-            productId: node.id,
-          },
-        },
-        tries: 20,
-      });
-    }
-    for (const media of mediaNodes) {
-      if (media.preview?.image?.url) {
-        console.log('product id----> ',node.id)
-        console.log("found:", media.preview.image.url);
-
-      } else {
-        console.log("not-found");
-        // await gqlClient.query<any>({
-        //   data: {
-        //     query: `#graphql
-        //     mutation productCreateMedia($media: [CreateMediaInput!]!, $productId: ID!) {
-        //       productCreateMedia(media: $media, productId: $productId) {
-        //
-        //         mediaUserErrors {
-        //           field
-        //           message
-        //         }
-        //
-        //       }
-        //     }`,
-        //     variables: {
-        //       media: [
-        //         {
-        //           alt: 'package-protection',
-        //           mediaContentType: 'IMAGE',
-        //           originalSource: productImage,
-        //         },
-        //       ],
-        //       productId: node.id,
-        //     },
-        //   },
-        //   tries: 20,
-        // });
-      }
-    }
-  }
-
+export async function loader() {
   return json({ apiKey: process.env.SHOPIFY_API_KEY });
 }
 
