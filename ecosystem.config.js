1d379a8ea6d042e003dd12f4f7a72e392b1cde1e
WeNexusTeam <wenexusltd@gmail.com>
2024-12-28 19:14:18 +0600

    remove instances

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,4 +1,3 @@
-const cpus = require('os').cpus().length;
 const dotenv = require('dotenv');
 const fs = require('fs');
 
@@ -9,7 +8,6 @@ module.exports = {
       exec_mode: 'fork',
       increment_var: 'PORT',
       script: './node_modules/.bin/remix-serve build/index.js',
-      instances: cpus < 2 ? 2 : cpus,
       instance_var: 'NODE_ID',
       source_map_support: true,
       env: dotenv.parse(fs.readFileSync('.env')),
