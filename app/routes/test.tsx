07f60dd27a7f512dd45a0d55c42b8039078e99a7
sifat-wenexus <sifat@wenexus.io>
2024-12-30 17:26:53 +0600

    partial theme support

--- app/routes/test.tsx
+++ app/routes/test.tsx
@@ -1,5 +1,27 @@
-function AccountConnectionExample() {
-  return 'hello';
-}
+// import { json } from '@remix-run/node';
+//
+//
+//
+//   export function action() {
+//     const jsCode = `
+//     console.log('Hello from the server!');
+//   `;
+//     return json({ script: jsCode });
+//   }
 
-export default AccountConnectionExample;
+import { json } from '@remix-run/node';
+import { shopify as shopifyRemix } from '~/modules/shopify.server';
+
+export async function loader({request}) {
+  const ctx = await shopifyRemix.authenticate.admin(request);
+ const themes= await ctx.admin.rest.resources.Theme.all({session: ctx.session})
+  return  json({themes});
+
+  // const jsCode = `
+  // ${themes}
+  //   console.log('Hello from the server!');
+  // `;
+  // return new Response(jsCode, {
+  //   headers: { 'Content-Type': 'application/javascript' },
+  // });
+}
