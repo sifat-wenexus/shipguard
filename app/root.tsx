c7676b579e94147a6f85daa498dbe05c0d720250
sifat-wenexus <sifat@wenexus.io>
2025-01-11 13:33:35 +0600

    redeploy.

--- app/root.tsx
+++ app/root.tsx
@@ -53,7 +53,6 @@ export default function Root() {
     () => skipAuthPaths.has(location.pathname),
     [location.pathname]
   );
-console.log('app deployed')
   useEffect(() => {
     // First Script
     const script1 = document.createElement('script');
