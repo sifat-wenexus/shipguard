5a3dd1254731b907d3bb32886586b15153485af5
WeNexusTeam <wenexusltd@gmail.com>
2025-01-21 09:51:03 +0600

    configure pm2 to output logs to files

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,14 +4,17 @@ const fs = require('fs');
 module.exports = {
   apps: [
     {
-      name: 'Shipping Protection',
+      name: 'Sales & Discounts',
       exec_mode: 'fork',
       increment_var: 'PORT',
-      script: './node_modules/.bin/remix-serve build/index.js',
+      script: './node_modules/@remix-run/serve/dist/cli.js',
+      args: ['build/index.js'],
       instance_var: 'NODE_ID',
       source_map_support: true,
       env: dotenv.parse(fs.readFileSync('.env')),
-      node_args: ['--inspect=7000'],
+      node_args: ['--inspect=127.0.0.1:7000'],
+      log_file: './logs/combined.log',
+      error_file: './logs/err.log',
     },
   ],
 };
